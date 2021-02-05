09f51a97210efdb3c390830340aebd49d1dfb3c0
YahiroRyo <honakafn@gmail.com>
2021-02-05 18:06:13 +0900

    そろばん教室フラッシュ暗算

---
+++ js/flashMain.js
@@ -0,0 +1,80 @@
+let url;
+let params;
+let countInterval;
+let mainProcessInterval;
+let mainProcessCount = 0;
+let countNum = 3;
+let sum = 0;
+
+const count = () => {
+  const sound = new Audio('../audio/click.mp3');
+  sound.play();
+  const textElement = document.querySelector('.container_text');
+  textElement.innerHTML = countNum;
+  setTimeout(() => {
+    const textElement = document.querySelector('.container_text');
+    textElement.innerHTML = "";
+  }, 500)
+  countNum--;
+  if (countNum == 0) {
+    clearInterval(countInterval);
+    let bwtween = 0;
+    if (parseFloat(params.get('between')) * 1000 >= 1000) {
+      bwtween = 0
+    } else {
+      bwtween = parseFloat(params.get('between'));
+    }
+    setTimeout(() => {
+      mainProcessInterval = setInterval(mainProcess, parseFloat(params.get('between')) * 1000)
+    }, 1000 - bwtween);
+    countNum = 3;
+  }
+}
+const mainProcess = () => {
+  mainProcessCount++;
+  const sound = new Audio('../audio/route.mp3');
+  sound.play();
+  if (parseInt(params.get('timenum')) == mainProcessCount) {
+    mainProcessCount = 0;
+    clearInterval(mainProcessInterval);
+    setTimeout(() => {
+      const containerElement = document.querySelector('.container');
+      containerElement.style = "background-color: white;";
+      containerElement.innerHTML = '<div class="form"><label class="form_label">答えを入力</label><input class="form_input" type="text"><button class="form_button">回答</button></div>'
+      const formButtonElement = document.querySelector('.form_button');
+      formButtonElement.onclick = () => {
+        const formInput = document.querySelector('.form_input');
+        if (parseInt(formInput.value) == sum) {
+          containerElement.innerHTML = '<div class="form"><label class="form_label">正解!</label><button onclick="window.location.reload();" class="form_button">もう一度挑戦</button><button onclick="window.location.href = \'../index.html\';" class="form_button">設定変更</button></div>'
+          const sound = new Audio('../audio/true.mp3');
+          sound.play();
+        } else {
+          containerElement.innerHTML = '<div class="form"><label class="form_label">不正解!</label><label class="form_label">正解は・・・' + String(sum) +'</label><button onclick="window.location.reload();" class="form_button">もう一度挑戦</button><button onclick="window.location.href = \'../index.html\';" class="form_button">設定変更</button></div>'
+          const sound = new Audio('../audio/false.mp3');
+          sound.play();
+        }
+      }
+    }, (parseFloat(params.get('between')) * 1000) / 2);
+  }
+  let result = '';
+  for (let i = 0; i < parseInt(params.get('digitnum')); i++) {
+    let resultMem = Math.floor(Math.random() * 10);
+    if (resultMem != 0)
+      result += String(resultMem);
+    else
+      i--;
+  }
+  sum += parseInt(result);
+  const textElement = document.querySelector('.container_text');
+  textElement.innerHTML = result;
+  setTimeout(() => {
+    const textElement = document.querySelector('.container_text');
+    textElement.innerHTML = "";
+  }, (parseFloat(params.get('between')) * 1000) / 2);
+}
+window.addEventListener('load', () => {
+  url = new URL(window.location.href);
+  params = url.searchParams;
+  document.title = params.get('digitnum') +"桁" + params.get('timenum') + "枚" + params.get('between') + "秒";
+  countInterval = setInterval(count, 1000);
+});
---
+++ js/index.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', () => {
+  const submitElement = document.querySelector('.form_button');
+  submitElement.onclick = () => {
+    let isError = false;
+    const formInputElements = document.querySelectorAll('.form_input');
+    for (let i = 0; i < formInputElements.length; i++) {
+      if (!formInputElements[i].value) {
+        isError = true
+        const formErrorElements = document.querySelectorAll('.form_label__error');
+        for (let j = 0; j < formErrorElements.length; j++) {
+          if (j == i) { formErrorElements[j].innerHTML = "値が入力されていません"; }
+        }
+      }
+    }
+    if (!isError) {
+      window.location.href = "pages/flashMain.html?between="
+                              + formInputElements[0].value + "&timenum="
+                              + formInputElements[1].value + "&digitnum="
+                              + formInputElements[2].value;
+    }
+  }
+});
